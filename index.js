81290ddcfc7b6b0db29efabfafb8d78223d668ff
tumluliu <nudtlliu@gmail.com>
2015-06-23 20:39:38 +0800

    update to mmrp

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 'use strict';
 
-if (!L.mapbox) throw new Error('include mapbox.js before mapbox.directions.js');
+if (!L.mapbox) throw new Error('include mapbox.js before mmrp.js');
 
-L.mapbox.directions = require('./src/directions');
-L.mapbox.directions.format = require('./src/format');
-L.mapbox.directions.layer = require('./src/layer');
-L.mapbox.directions.inputControl = require('./src/input_control');
-L.mapbox.directions.errorsControl = require('./src/errors_control');
-L.mapbox.directions.routesControl = require('./src/routes_control');
-L.mapbox.directions.instructionsControl = require('./src/instructions_control');
+L.mmrp = require('./src/directions');
+L.mmrp.format = require('./src/format');
+L.mmrp.layer = require('./src/layer');
+L.mmrp.inputControl = require('./src/input_control');
+L.mmrp.errorsControl = require('./src/errors_control');
+L.mmrp.routesControl = require('./src/routes_control');
+L.mmrp.instructionsControl = require('./src/instructions_control');
