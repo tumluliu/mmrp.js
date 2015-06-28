318e55e0980c9c9ac8d38f2bdbcca190516cdbb5
tumluliu <nudtlliu@gmail.com>
2015-06-29 07:48:06 +0800

    use d3-xhr to send POST request, get rid of corslite

---
+++ src/d3_post.js
@@ -0,0 +1,33 @@
+'use strict';
+
+var d3xhr = require('d3-xhr');
+
+function d3post(url, reqData, callback, cors) {
+    var sent = false;
+
+    if (typeof cors === 'undefined') {
+        var m = url.match(/^\s*https?:\/\/[^\/]*/);
+        cors = m && (m[0] !== location.protocol + '//' + location.hostname +
+                (location.port ? ':' + location.port : ''));
+    }
+
+    var respData;
+
+    d3xhr.xhr(url)
+        .header("Content-Type", "application/json")
+        .post(
+                JSON.stringify(reqData),
+                function(err, rawData){
+                    respData = rawData;
+                    callback.call(err, respData, null);
+                }
+             );
+
+    function isSuccessful(status) {
+        return status >= 200 && status < 300 || status === 304;
+    }
+
+    return respData;
+}
+
+if (typeof module !== 'undefined') module.exports = d3post;
