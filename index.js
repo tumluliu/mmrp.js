8b45d6513d77e0ccf2cb5032371224dc9c13d1dc
tumluliu <nudtlliu@gmail.com>
2015-07-19 20:09:27 +0800

    add things for tracks control

--- index.js
+++ index.js
@@ -9,3 +9,4 @@ L.mmrp.inputControl = require('./src/input_control');
 L.mmrp.errorsControl = require('./src/errors_control');
 L.mmrp.routesControl = require('./src/routes_control');
 L.mmrp.instructionsControl = require('./src/instructions_control');
+L.mmrp.tracksControl = require('./src/tracks_control.js');
